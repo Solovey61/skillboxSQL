477b65360ea60e680035c25f9dc76c396f2ef512
Michael Solovey <solovey.0408@gmail.com>
2019-09-04 23:38:40 +0300

    Additional task realize;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,15 +12,22 @@ public class Main {
 		String password = mysqlProperties.getProperty("password");
 		Connection connection = DriverManager.getConnection(url, user, password);
 		Statement statement = connection.createStatement();
-		System.out.println("Average purchase count for the year: ");
+
 		ResultSet resultSet = statement.executeQuery("SELECT course_name, count_summary/12 FROM (SELECT course_name, COUNT(*) count_summary FROM PurchaseList GROUP BY course_name) subq GROUP BY course_name;");
-		while (resultSet.next())
-			System.out.printf("%-40s|%10s\n", resultSet.getString(1), resultSet.getString(2));
-		System.out.println();
-		System.out.println("Average purchase count for the period: ");
+			printResult(resultSet, "Average purchase count for the year: ");
+
 		resultSet = statement.executeQuery("SELECT course_name, AVG(count_summary) FROM (SELECT course_name, COUNT(*) count_summary FROM PurchaseList GROUP BY course_name, MONTH(subscription_date)) subq GROUP BY course_name;");
+			printResult(resultSet, "Average purchase count for the period:");
+
+		resultSet = statement.executeQuery("SELECT course_name, purchases/months FROM (SELECT course_name, PERIOD_DIFF(EXTRACT(YEAR_MONTH FROM MAX(subscription_date)), EXTRACT(YEAR_MONTH FROM MIN(subscription_date)))-1 months, COUNT(*) purchases FROM PurchaseList GROUP BY course_name) subq;");
+		printResult(resultSet, "Average purchase count for the period from first purchase to last: ");
+
+		connection.close();
+	}
+
+	public static void printResult(ResultSet resultSet, String title) throws SQLException {
+		System.out.printf("\n%s\n", title);
 		while (resultSet.next())
 			System.out.printf("%-40s|%10s\n", resultSet.getString(1), resultSet.getString(2));
-		connection.close();
 	}
 }
